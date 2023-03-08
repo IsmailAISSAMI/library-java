eb8e2a5421e4eea23d3a1b640fc95b5044e474f2
ismailaissami <aissami.ismail@gmail.com>
2023-03-08 23:13:27 +0100

    from question 1 to question 2c

---
+++ src/library/Book.java
@@ -0,0 +1,123 @@
+package library;
+
+public class Book {
+    private String title;
+    private String authorFirstName;
+    private String authorLastName;
+    private Category category; // this is an enum
+
+    private int isbn;
+    private String registrationCode;
+
+    //BOOK CATEGORIES
+    public enum Category {
+        DETECTIVE, NOVEL, JUNIOR, PHILOSOPHY, SCIENCE_FICTION
+    }
+
+    //CONSTRUCTORS
+    public Book() {
+    }
+
+    public Book(String title, String authorFirstName,String authorLastName,Category category,  int isbn  , String registrationCode) {
+        this.title = title;
+        this.authorFirstName = authorFirstName;
+        this.authorLastName = authorLastName;
+        this.category = category;
+        this.isbn = isbn;
+        this.registrationCode = registrationCode;
+    }
+
+    //GETTERS
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthorFirstName() {
+        return authorFirstName;
+    }
+
+    public String getAuthorLastName() {
+        return authorLastName;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public String getRegistrationCode() {
+        return registrationCode;
+    }
+
+    //SETTERS
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthorFirstName(String authorFirstName) {
+        this.authorFirstName = authorFirstName;
+    }
+
+    public void setAuthorLastName(String authorLastName) {
+        this.authorLastName = authorLastName;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    // // MADE IN QUESTION 1.A
+    // public void setRegistrationCode(String registrationCode) {
+    //     this.registrationCode = registrationCode;
+    // }
+
+    // Question 2.C:  
+    public void setRegistrationCode() {
+        // Two first letters of the author's first and last name
+        String authorInitials = this.authorLastName.substring(0, 2).toUpperCase() + this.authorFirstName.substring(0, 2).toUpperCase();
+        // The first two letters of the book's category
+        String categoryCode = "";
+        switch (this.category) {
+            case DETECTIVE:
+                categoryCode = "DE";
+                break;
+            case NOVEL:
+                categoryCode = "NO";
+                break;
+            case JUNIOR:
+                categoryCode = "JU";
+                break;
+            case PHILOSOPHY:
+                categoryCode = "PH";
+                break;
+            case SCIENCE_FICTION:
+                categoryCode = "SC";
+                break;
+        }
+        // The last two digits of the book's ISBN
+        int isbnLastTwoDigits = this.isbn % 100;
+        this.registrationCode = authorInitials + categoryCode + isbnLastTwoDigits;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + "title=" + title + ", authorFirstName=" + authorFirstName + ", authorLastName=" + authorLastName + ", category=" + category + ", isbn=" + isbn + ", registrationCode=" + registrationCode + '}';
+    }
+
+    public void displayBook() {
+        System.out.println("Book = {\n" + "\tTitle=" + title + ",\n\tAuthor firstName=" + authorFirstName + ",\n\tAuthor lastName=" + authorLastName + ",\n\tCategory=" + category + ",\n\tISBN=" + isbn + ",\n\tregistration code=" + registrationCode + "\n}");
+    }
+
+    
+    
+    
+}
---
+++ src/library/Main.java
@@ -0,0 +1,95 @@
+package library;
+import java.util.Scanner;
+
+public class Main {
+    // Question 2.b: Display the characteristics of the book object
+    public static void displayBookCharacteristics(Book book) {
+        System.out.println("Title: " + book.getTitle());
+        System.out.println("Author: " + book.getAuthorFirstName() + " " + book.getAuthorLastName());
+        System.out.println("Category: " + book.getCategory());
+        System.out.println("ISBN: " + book.getIsbn());
+        System.out.println("Registration code: " + book.getRegistrationCode());
+    }
+
+    //verify if the input length is greater than 2 characters because the registration code must be at least 2 characters
+    public static boolean verifyInputLength(String input) {
+        if (input.length() < 2) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean verifyISBNInputLength(int number) {
+        if (number < 10) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // test class Book
+        Book book = new Book("The Lord of the Rings", "J.R.R.", "Tolkien", Book.Category.NOVEL, 123456789, "ABC123");
+        System.out.println(book);
+
+        // Question 1.b        
+        Book pocketBook = new Book();
+
+        System.out.println("********** Welcome to my library **********");
+        System.out.println("Enter the title of the book: ");
+        pocketBook.setTitle(scanner.nextLine());
+        
+        System.out.println("Enter the first name of the author: ");
+        String firstName = scanner.nextLine();
+        while (!verifyInputLength(firstName)) {
+            System.out.println("Invalid input. Please enter at least 2 characters: ");
+            firstName = scanner.nextLine();
+        }
+        pocketBook.setAuthorFirstName(firstName);
+        
+        System.out.println("Enter the last name of the author: ");
+        String lastName = scanner.nextLine();
+        while (!verifyInputLength(lastName)) {
+            System.out.println("Invalid input. Please enter at least 2 characters: ");
+            lastName = scanner.nextLine();
+        }
+        pocketBook.setAuthorLastName(lastName);
+        
+        System.out.println("Enter the category of the book: ");
+        String categoryInput = scanner.nextLine().toUpperCase();
+        // While the category is not valid, we continue to ask the user to enter a valid category
+        while (!categoryInput.equals("DETECTIVE") && !categoryInput.equals("NOVEL") && !categoryInput.equals("JUNIOR") && !categoryInput.equals("PHILOSOPHY") && !categoryInput.equals("SCIENCE_FICTION")) {
+            System.out.println("Invalid category. Please enter a valid category [DETECTIVE, NOVEL, JUNIOR, PHILOSOPHY, SCIENCE_FICTION]:");
+            categoryInput = scanner.nextLine().toUpperCase();
+        }
+        pocketBook.setCategory(Book.Category.valueOf(categoryInput));
+        
+        System.out.println("Enter the ISBN of the book: (int)");
+        int isbn = scanner.nextInt();
+        while (!verifyISBNInputLength(isbn)) {
+            System.out.println("Invalid input. Please enter at least 2 digits: ");
+            isbn = scanner.nextInt();
+        }
+        pocketBook.setIsbn(isbn);
+        
+        // Changed in Question 2.C
+        // System.out.println("Enter the registration code of the book: (String)");
+        // pocketBook.setRegistrationCode(scanner.next());
+        pocketBook.setRegistrationCode();
+        
+        System.out.println("[INFO] Book created successfully!");
+        System.out.println("[DEBUG] " + pocketBook.toString());
+        
+        // Question 2.A:
+        pocketBook.displayBook();
+
+        // Question 2.B:
+        displayBookCharacteristics(pocketBook);
+
+        System.out.println("********** thank you for using my library **********");
+    }
+    
+}
+
